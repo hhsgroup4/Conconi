602d26bae3ae86cc4cea88a2de8feb02175dace5
va-14 <vince1@live.nl>
2015-05-26 16:05:51 +0200

    Main buttons/ adviceactivity / languagesstrings
    Added te buttons on the mainactivity
    Added an adviceactivity
    Added dutch language file and spanish

--- ConconiIndibase/app/src/main/java/com/indibase/conconi/app/MainActivity.java
+++ ConconiIndibase/app/src/main/java/com/indibase/conconi/app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.indibase.conconi.app;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.indibase.conconi.R;
 
 
@@ -10,6 +13,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void advicesView(View view){
+        Intent intent = new Intent(this, AdvicesActivity.class);
+        startActivity(intent);
+    }
+    public void historyView(View view){
+       /* Intent intent = new Intent(this, History.class);
+        startActivity(intent);*/
+    }
+    public void beginTest(View view){
+      /*  Intent intent = new Intent(this, BeginTest.class);
+        startActivity(intent);*/
     }
 
 }
