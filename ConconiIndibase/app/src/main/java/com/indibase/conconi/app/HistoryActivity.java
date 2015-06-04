e676c5325447fca3b3323ae21f818e12368b4885
va-14 <vince1@live.nl>
2015-06-04 02:25:50 +0200

    Measurements testdata fix and start of history

--- ConconiIndibase/app/src/main/java/com/indibase/conconi/app/HistoryActivity.java
+++ ConconiIndibase/app/src/main/java/com/indibase/conconi/app/HistoryActivity.java
@@ -1,16 +1,62 @@
 package com.indibase.conconi.app;
 
+import android.content.CursorLoader;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.indibase.conconi.R;
 
+import java.util.ArrayList;
+
+import static com.indibase.conconi.R.layout.activity_history;
+
 public class HistoryActivity extends ActionBarActivity {
 
+    String[] historyItems;
+    ListView listViewHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        setContentView(activity_history);
+        historyItems = getTests();
+
+        listViewHistory = (ListView) findViewById(R.id.listview_history);
+        fillListView(historyItems);
+
+
+    }
+    private void fillListView(String[] historyItems){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, historyItems);
+        listViewHistory.setAdapter(adapter);
+    }
+
+    private String[] getTests(){
+        Uri tests = Uri.parse("content://com.indibase.provider.conconi/test");
+        Cursor c;
+        CursorLoader cursorLoader = new CursorLoader(this,tests,null,null,null,null);
+        c = cursorLoader.loadInBackground();
+        historyItems = null;
+        //historyItems = new String[c.getCount()][3];
+        historyItems = new String[c.getCount()];
+        int index = 0;
+
+        while (c.moveToNext()) {
+            if(index == 0)
+                c.moveToFirst();
+          //  historyItems[index][0] = c.getString(0);
+            //historyItems[index][1] = c.getString(1);
+            historyItems[index] = "ID: " + c.getString(0) + " Time: " + c.getString(1) + " BPM: "  + c.getString(2);
+           // historyItems[index][2] = c.getString(2);
+            index++;
+        }
+        return historyItems;
     }
 }
--- ConconiIndibase/app/src/main/java/com/indibase/conconi/provider/schemas/Measurement.java
+++ ConconiIndibase/app/src/main/java/com/indibase/conconi/provider/schemas/Measurement.java
@@ -29,103 +29,104 @@ public class Measurement extends DatabaseSchema implements BaseColumns{
 
     @Override
     public String getInsertSql() {
-        return "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 0, 110);"+
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 10, 110)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 20, 110)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 30, 111)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 40, 111)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 50, 111)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 60, 111)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 70, 111)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 80, 111)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 90, 112)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 100, 112)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 110, 113)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 120, 114)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 130, 114)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 140, 113)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 150, 113)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 160, 113)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 170, 113)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 180, 113)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 190, 114)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 200, 114)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 210, 115)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 220, 116)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 230, 116)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 240, 117)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 250, 118)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 260, 119)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 270, 120)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 280, 121)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 290, 123)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 300, 124)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 310, 123)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 320, 123)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 330, 122)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 340, 121)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 350, 120)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 360, 119)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 370, 121)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 380, 123)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 390, 125)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 400, 126)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 410, 129)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 420, 131)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 430, 132)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 440, 132)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 450, 133)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 460, 134)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 470, 135)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 480, 136)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 490, 137)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 500, 138)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 510, 140)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 520, 141)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 530, 143)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 540, 145)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 550, 147)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 560, 149)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 570, 151)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 580, 152)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 590, 154)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 600, 156)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 610, 157)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 620, 158)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 630, 160)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 640, 161)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 650, 162)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 660, 163)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 670, 164)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 680, 165)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 690, 166)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 700, 167)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 710, 168)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 720, 170)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 730, 170)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 740, 171)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 750, 172)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 760, 172)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 770, 173)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 780, 174)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 790, 174)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 800, 175)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 810, 176)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 820, 176)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 830, 177)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 840, 177)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 850, 177)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 860, 178)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 870, 179)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 880, 179)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 890, 180)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 900, 181)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 910, 182)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 920, 182)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 930, 183)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 940, 184)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 950, 185)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 960, 186)" +
-        "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) VALUES (1, 964, 186)";
+        return "INSERT INTO `" + getTableName() + "` (`test_id`, `second`, `bpm`) " +
+        "SELECT 1 AS `test_id`, 0 AS `second`, 110 AS `bpm` "+
+        "UNION SELECT 1, 10, 110 " +
+        "UNION SELECT 1, 20, 110 " +
+        "UNION SELECT 1, 30, 111 " +
+        "UNION SELECT 1, 40, 111 " +
+        "UNION SELECT 1, 50, 111 " +
+        "UNION SELECT 1, 60, 111 " +
+        "UNION SELECT 1, 70, 111 " +
+        "UNION SELECT 1, 80, 111 " +
+        "UNION SELECT 1, 90, 112 " +
+        "UNION SELECT 1, 100, 112 " +
+        "UNION SELECT 1, 110, 113 " +
+        "UNION SELECT 1, 120, 114 " +
+        "UNION SELECT 1, 130, 114 " +
+        "UNION SELECT 1, 140, 113 " +
+        "UNION SELECT 1, 150, 113 " +
+        "UNION SELECT 1, 160, 113 " +
+        "UNION SELECT 1, 170, 113 " +
+        "UNION SELECT 1, 180, 113 " +
+        "UNION SELECT 1, 190, 114 " +
+        "UNION SELECT 1, 200, 114 " +
+        "UNION SELECT 1, 210, 115 " +
+        "UNION SELECT 1, 220, 116 " +
+        "UNION SELECT 1, 230, 116 " +
+        "UNION SELECT 1, 240, 117 " +
+        "UNION SELECT 1, 250, 118 " +
+        "UNION SELECT 1, 260, 119 " +
+        "UNION SELECT 1, 270, 120 " +
+        "UNION SELECT 1, 280, 121 " +
+        "UNION SELECT 1, 290, 123 " +
+        "UNION SELECT 1, 300, 124 " +
+        "UNION SELECT 1, 310, 123 " +
+        "UNION SELECT 1, 320, 123 " +
+        "UNION SELECT 1, 330, 122 " +
+        "UNION SELECT 1, 340, 121 " +
+        "UNION SELECT 1, 350, 120 " +
+        "UNION SELECT 1, 360, 119 " +
+        "UNION SELECT 1, 370, 121 " +
+        "UNION SELECT 1, 380, 123 " +
+        "UNION SELECT 1, 390, 125 " +
+        "UNION SELECT 1, 400, 126 " +
+        "UNION SELECT 1, 410, 129 " +
+        "UNION SELECT 1, 420, 131 " +
+        "UNION SELECT 1, 430, 132 " +
+        "UNION SELECT 1, 440, 132 " +
+        "UNION SELECT 1, 450, 133 " +
+        "UNION SELECT 1, 460, 134 " +
+        "UNION SELECT 1, 470, 135 " +
+        "UNION SELECT 1, 480, 136 " +
+        "UNION SELECT 1, 490, 137 " +
+        "UNION SELECT 1, 500, 138 " +
+        "UNION SELECT 1, 510, 140 " +
+        "UNION SELECT 1, 520, 141 " +
+        "UNION SELECT 1, 530, 143 " +
+        "UNION SELECT 1, 540, 145 " +
+        "UNION SELECT 1, 550, 147 " +
+        "UNION SELECT 1, 560, 149 " +
+        "UNION SELECT 1, 570, 151 " +
+        "UNION SELECT 1, 580, 152 " +
+        "UNION SELECT 1, 590, 154 " +
+        "UNION SELECT 1, 600, 156 " +
+        "UNION SELECT 1, 610, 157 " +
+        "UNION SELECT 1, 620, 158 " +
+        "UNION SELECT 1, 630, 160 " +
+        "UNION SELECT 1, 640, 161 " +
+        "UNION SELECT 1, 650, 162 " +
+        "UNION SELECT 1, 660, 163 " +
+        "UNION SELECT 1, 670, 164 " +
+        "UNION SELECT 1, 680, 165 " +
+        "UNION SELECT 1, 690, 166 " +
+        "UNION SELECT 1, 700, 167 " +
+        "UNION SELECT 1, 710, 168 " +
+        "UNION SELECT 1, 720, 170 " +
+        "UNION SELECT 1, 730, 170 " +
+        "UNION SELECT 1, 740, 171 " +
+        "UNION SELECT 1, 750, 172 " +
+        "UNION SELECT 1, 760, 172 " +
+        "UNION SELECT 1, 770, 173 " +
+        "UNION SELECT 1, 780, 174 " +
+        "UNION SELECT 1, 790, 174 " +
+        "UNION SELECT 1, 800, 175 " +
+        "UNION SELECT 1, 810, 176 " +
+        "UNION SELECT 1, 820, 176 " +
+        "UNION SELECT 1, 830, 177 " +
+        "UNION SELECT 1, 840, 177 " +
+        "UNION SELECT 1, 850, 177 " +
+        "UNION SELECT 1, 860, 178 " +
+        "UNION SELECT 1, 870, 179 " +
+        "UNION SELECT 1, 880, 179 " +
+        "UNION SELECT 1, 890, 180 " +
+        "UNION SELECT 1, 900, 181 " +
+        "UNION SELECT 1, 910, 182 " +
+        "UNION SELECT 1, 920, 182 " +
+        "UNION SELECT 1, 930, 183 " +
+        "UNION SELECT 1, 940, 184 " +
+        "UNION SELECT 1, 950, 185 " +
+        "UNION SELECT 1, 960, 186 " +
+        "UNION SELECT 1, 964, 186";
     }
 }
