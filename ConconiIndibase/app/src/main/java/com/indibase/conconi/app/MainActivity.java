86e5d2eee538a47f0838fe33020a8e63e82bbe9a
cappi10 <ralph_freeke@hotmail.com>
2015-06-04 16:39:56 +0200

    Added db examples in MainActivity

--- ConconiIndibase/app/src/main/java/com/indibase/conconi/app/MainActivity.java
+++ ConconiIndibase/app/src/main/java/com/indibase/conconi/app/MainActivity.java
@@ -16,25 +16,13 @@ import com.indibase.conconi.R;
 
 public class MainActivity extends TabActivity {
 
-    ViewPager viewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Uri tests = Uri.parse("content://com.indibase.provider.conconi/test/including_measurements");
-        Cursor c;
-        CursorLoader cursorLoader = new CursorLoader(
-                this,
-                tests,
-                null,
-                null,
-                null,
-                null);
-        c = cursorLoader.loadInBackground();
-
-        Log.d("lifecycle", DatabaseUtils.dumpCursorToString(c));
+        getTestExample(1);
+        //getTestWithAllMeasurementsExample(1);
 
         // create the TabHost that will contain the Tabs
         TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
@@ -58,6 +46,38 @@ public class MainActivity extends TabActivity {
         tabHost.addTab(tab3);
 
         tabHost.setCurrentTab(2);
+    }
+
+    //will get test without any linked measurements
+    private void getTestExample(int id){
+        Uri tests = Uri.parse("content://com.indibase.provider.conconi/test/" + id);
+        Cursor c;
+        CursorLoader cursorLoader = new CursorLoader(
+                this,
+                tests,
+                null,
+                null,
+                null,
+                null);
+        c = cursorLoader.loadInBackground();
+
+        Log.d("lifecycle", DatabaseUtils.dumpCursorToString(c));
+    }
+
+    //will get test AND MEASUREMENTS with provided test_id
+    private void getTestWithAllMeasurementsExample(int id){
+        Uri tests = Uri.parse("content://com.indibase.provider.conconi/test/including_measurements");
+        Cursor c;
+        CursorLoader cursorLoader = new CursorLoader(
+                this, //context
+                tests, //uri
+                null, //projection: what columns to retrieve (default: all of them)
+                "test_id = ?", // Where clause
+                new String[] {String.valueOf(id)}, //Arguments of where clause (fills in the questionmarks in order)
+                null); //sorting here (for example: "creation DESC"
+        c = cursorLoader.loadInBackground();
+
+        Log.d("lifecycle", DatabaseUtils.dumpCursorToString(c));
     }
 
 
