ae73e91d25e9011a198fe9b8c0b9df90a8758144
va-14 <vince1@live.nl>
2015-06-04 13:12:20 +0200

    OneTest get function

--- ConconiIndibase/app/src/main/java/com/indibase/conconi/models/DbTest.java
+++ ConconiIndibase/app/src/main/java/com/indibase/conconi/models/DbTest.java
@@ -15,6 +15,30 @@ import java.util.Date;
  * Created by Vince on 4-6-2015.
  */
 public class DbTest {
+    public static Test getTest(Activity activity, int identifier){
+        Uri oneTest = Uri.parse("content://com.indibase.provider.conconi/test/"+identifier);
+        Cursor c;
+        CursorLoader cursorLoader = new CursorLoader(activity,oneTest,null,null,null,null);
+        c = cursorLoader.loadInBackground();
+
+        Test test = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        while (c.moveToNext()) {
+            c.moveToFirst();
+
+            int id = Integer.valueOf(c.getString(0));;
+            Date date = new Date();
+            int deflection = Integer.valueOf(c.getString(2));
+            try {
+                date = dateFormat.parse(c.getString(1));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            test = new Test(id, date, deflection);
+        }
+        return test;
+    }
 
     public static ArrayList<String> getAllTestString(Activity activity){
         ArrayList<String> tests = getAll(activity, "testString", String.class);
@@ -36,6 +60,7 @@ public class DbTest {
         ArrayList<Integer> tests = getAll(activity, "deflection", Integer.class);
         return tests;
     }
+
     private static <T> ArrayList<T> getAll(Activity activity, String typeStr, Class<T> type){
         Uri allTests = Uri.parse("content://com.indibase.provider.conconi/test");
         Cursor c;
@@ -74,8 +99,6 @@ public class DbTest {
                     t = new Integer(deflection);
                     break;
             }
-
-
             tests.add((T) t);
         }
         return tests;
